e074badc63e92c1de1dbfe8c0656070a58e27744
Gideon Okuro <gideonollonde@gmail.com>
2020-07-05 16:17:02 +0300

    create a test method to get the squad name

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -2,12 +2,18 @@ package models;
 
 public class Squad {
     private int max_size;
+    private String name;
 
     public Squad(int max_size, String name, String Cause) {
         this.max_size = max_size;
+        this.name = name;
     }
 
     public int getMax_size() {
         return max_size;
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -15,4 +15,10 @@ public class SquadTest {
         Squad squad =  new Squad(5, "Squad 1","Avengers Assemble");
         assertEquals(5, squad.getMax_size());
     }
+
+    @Test
+    public void saves_squadName_squad1(){
+        Squad squad =  new Squad(5, "Squad 1","Avengers Assemble");
+        assertEquals("Squad 1", squad.getName());
+    }
 }

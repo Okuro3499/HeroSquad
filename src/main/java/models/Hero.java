15759ab2c4bcfa6d9fb5489dac4bf8eb14b73cec
Gideon Okuro <gideonollonde@gmail.com>
2020-07-05 14:00:27 +0300

    run test to intantiate Hero

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,7 @@
 package models;
 
 public class Hero {
+    public Hero(String name, int age, String special_power, String weakness) {
+
+    }
 }
---
+++ src/test/java/models/HeroTest.java
@@ -0,0 +1,14 @@
+package models;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HeroTest {
+    @Test
+    public void createsInstanceOfHero(){
+        Hero hero = new Hero ("Storm",26,"Weather modification","Emotionally unstable");
+        assertTrue(hero instanceof Hero);
+    }
+
+
+}
